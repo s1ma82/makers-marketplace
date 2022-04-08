1c220f3e800e1a916f6e4b137d6ae0f3844f581a
S1ma <s1mahochukvas@vk.com>
2022-04-08 14:44:32 +0300

    Startups-block - adapted

--- src/Components/video-container/video-comp.jsx
+++ src/Components/video-container/video-comp.jsx
@@ -4,7 +4,7 @@ import styles from './video.module.scss'
 const VideoComp = () => {
     return (
         <div className={`${styles.video_wrapper}`}>
-         <iframe width="560" height="315" src="https://www.youtube.com/embed/p94QPaI4SxE" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/p94QPaI4SxE" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
         </div>
     );
 }
