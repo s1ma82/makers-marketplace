da8c07377caf2f889a8b763aedbc667c9dd199ca
S1ma <s1mahochukvas@vk.com>
2022-04-08 18:12:12 +0300

    Modal-window adapted

--- src/Components/startups/modal/modal-comp.jsx
+++ src/Components/startups/modal/modal-comp.jsx
@@ -10,25 +10,26 @@ const ModalComp = () => {
         const {title, descr, users, price, mrr} = modal.obj
         return (
             <div id='modal' className={styles.modal}>
-                <div className={styles.content}>
-                </div>
+                <div className={styles.content}></div>
                 <div className={styles.info}>
                     <h1 className={styles.price}>{price}</h1>
                     <div className={styles.info_item}>
                         <h1 className={styles.title}>{title}</h1>
                         <p className={styles.descr}>{descr}</p>
                     </div>
-                    <div className={styles.info_item}>
-                        <span>{users}</span>
-                        <p>users</p>
-                    </div>
-                    <div className={styles.info_item}>
-                        <span>{mrr}</span>
-                        <p>mrr</p>
-                    </div>
-                    <div className={styles.info_item} style={{gridArea: '2 / 1 / 3 / 3'}}>
-                        <span>{price}</span>
-                        <p>price</p>
+                    <div className={styles.info_stats}>
+                        <div className={styles.info_stats_item}>
+                            <span>{users}</span>
+                            <p>users</p>
+                        </div>
+                        <div className={styles.info_stats_item}>
+                            <span>{mrr}</span>
+                            <p>mrr</p>
+                        </div>
+                        <div className={styles.info_stats_item}>
+                            <span>{price}</span>
+                            <p>price</p>
+                        </div>
                     </div>
                     <button id='hidemodalBtn' className={styles.modal_hidebtn} onClick={() => dispatch(hideModal())}>X</button>
                     <div className={styles.info_item}>
